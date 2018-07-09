2689d6d2e2729dbe48f564b8ac8316068a143fd6
blackmiaool <blackmiaool@gmail.com>
2018-07-09 17:19:58 +0800

    it works

---
+++ index.js
@@ -0,0 +1,113 @@
+const URL=require('url');
+
+function parse(url){
+    const ret=URL.parse(url,true);
+    delete ret.search;
+    return ret;
+}
+export default class YoupinImg {
+    constructor(url) {    
+        const base=typeof document==='undefined'?'https://':location.href;
+        url=URL.resolve('https://',url);
+        this.a = parse(url);
+        if (url.match(/@base/)) {
+            this.mode = "kingsoft";
+        } else if (/app\/shop\/img\?/.test(url)) {
+            this.mode = "standard";
+        } else {
+            this.mode = "unkown";
+        }
+    }
+    get url(){
+        return URL.format(this.a); 
+    }
+    set url(value){
+        console.warn("u can't set its url");
+    }
+    setWebp() {
+        switch (this.mode) {
+            case "standard":
+                this.standardAddParam('t','webp');
+                break;
+            case "kingsoft": {
+                this.kingsoftAddParam('F','webp');
+                break;
+            }
+        }
+        return this;
+    }
+    setSize(w, h) {
+        switch (this.mode) {
+            case "standard":
+                this.standardAddParams({ w, h });
+                break;
+            case "kingsoft": {
+                let inner = {
+                    w: this.kingsoftGetParam("w"),
+                    h: this.kingsoftGetParam("h")
+                };
+                if (inner.w && inner.h) {
+                    let scalew =w / this.kingsoftGetParam("etw")||1;
+                    let scaleh=h / this.kingsoftGetParam("eth")||1;
+                    this.kingsoftAddParams({
+                        w: Math.round(scalew * inner.w),
+                        h: Math.round(scaleh * inner.h)
+                    });
+                }
+                this.kingsoftAddParams({
+                    etw: w,
+                    eth: h
+                });
+
+                break;
+            }
+        }
+        return this;
+    }
+    getSize() {
+        if (this.mode === "standard") {
+            return {
+                w: this.standardGetParam("w"),
+                h: this.standardGetParam("h")
+            };
+        } else if (this.mode === "kingsoft") {
+            return {
+                w: this.kingsoftGetParam("etw"),
+                h: this.kingsoftGetParam("eth")
+            };
+        }
+    }
+    standardGetParam(key) {        
+        return this.a.query[key];        
+    }
+    kingsoftGetParam(key) {
+        return (this.a.pathname.match(new RegExp(`[?|&]${key}=(\\d+)`)) ||
+            [])[1];
+    }
+    standardAddParams(map) {
+        Object.assign(this.a.query,map);
+    }
+    standardAddParam(key, value) {
+        this.a.query[key]=value;
+    }
+    kingsoftAddParams(map) {
+        this.kingsoftAddParam(undefined, undefined, map);
+    }
+    kingsoftAddParam(key, value, map) {
+        
+        const action = (this.a.pathname.match(/@[\s\S]+?(&|$)/, "?") || [""])[0];
+        let pathname = this.a.pathname;
+        if (action) {
+            pathname = pathname.replace(action, "?");
+        }
+        let paramsMap = map || { [key]: value };
+        let pathnameObj=parse(pathname);
+        Object.assign(pathnameObj.query,paramsMap)
+        pathname=URL.format(pathnameObj);        
+        if (action) {
+            pathname = pathname.replace("?", action);
+        }
+        this.a.pathname = pathname;
+    }
+}
+
---
+++ test/test.js
@@ -0,0 +1,83 @@
+import YoupinImg from "../index";
+
+const chai = require('chai');
+ 
+const expect = chai.expect;
+
+chai.should();
+
+function addHash(url,hash){
+    return `${url}#${hash}`;
+}
+describe('standard mode', function () {
+    const testSizeW="500";
+    const testSizeH="800";    
+    const urlWithoutSize='https://shop.io.mi-img.com/app/shop/img?id=shop_b547304e56c54089601373e3840c0710.jpeg';
+    const urlWithSize=`${urlWithoutSize}&w=${testSizeW}&h=${testSizeH}`;
+    
+    const hash="bbbccc/dddeee";
+    it('it should be created', function (done) {
+        new YoupinImg(urlWithSize);
+        done();
+    });
+    it('it can be resized', function (done) {
+        new YoupinImg(urlWithoutSize).setSize(100,200).url.should.equal(`${urlWithoutSize}&w=100&h=200`);
+        new YoupinImg(urlWithSize).setSize(100,200).url.should.equal(`${urlWithoutSize}&w=100&h=200`);
+        new YoupinImg(addHash(urlWithoutSize,hash)).setSize(100,200).url.should.equal(addHash(`${urlWithoutSize}&w=100&h=200`,hash));
+        new YoupinImg(addHash(urlWithSize,hash)).setSize(100,200).url.should.equal(addHash(`${urlWithoutSize}&w=100&h=200`,hash));
+        done();
+    });
+    it('we can get its size', function (done) {
+        new YoupinImg(urlWithoutSize).getSize().should.deep.equal({
+            w:undefined,
+            h:undefined
+        });
+        new YoupinImg(urlWithSize).getSize().should.deep.equal({
+            w:testSizeW,
+            h:testSizeH
+        });
+        done();
+    }); 
+    it('we can make it webp', function (done) {
+        new YoupinImg(urlWithSize).setWebp().url.should.equal(urlWithSize+'&t=webp');
+        done();
+    });        
+});
+
+describe('kingsoft mode', function () {
+    const testSizeW="540";
+    const testSizeH="846";
+    const coreUrl=`img.youpin.mi-img.com/800_pic/6b5d945739be9da56980a39efb209574.png`;
+    const urlWithoutSize=`//${coreUrl}@base@tag=imgScale&et=1&etc=FFFFFF`;
+    const urlWithSize=`//${coreUrl}@base@tag=imgScale&h=350&w=550&et=1&eth=${testSizeH}&etw=${testSizeW}&etc=FFFFFF`;
+    
+    const hash="bbbccc/dddeee";
+    it('it should be created', function (done) {
+        new YoupinImg(urlWithSize);
+        done();
+    });
+    it('it can be resized', function (done) {
+        let result1='https://img.youpin.mi-img.com/800_pic/6b5d945739be9da56980a39efb209574.png@base@tag=imgScale&et=1&etc=FFFFFF&etw=100&eth=200';
+        let result2='https://img.youpin.mi-img.com/800_pic/6b5d945739be9da56980a39efb209574.png@base@tag=imgScale&h=83&w=102&et=1&eth=200&etw=100&etc=FFFFFF';
+        new YoupinImg(urlWithoutSize).setSize(100,200).url.should.equal(result1);
+        new YoupinImg(urlWithSize).setSize(100,200).url.should.equal(result2);
+        new YoupinImg(addHash(urlWithoutSize,hash)).setSize(100,200).url.should.equal(addHash(result1,hash));
+        new YoupinImg(addHash(urlWithSize,hash)).setSize(100,200).url.should.equal(addHash(result2,hash));
+        done();
+    });
+    it('we can get its size', function (done) {
+        new YoupinImg(urlWithoutSize).getSize().should.deep.equal({
+            w:undefined,
+            h:undefined
+        });
+        new YoupinImg(urlWithSize).getSize().should.deep.equal({
+            w:testSizeW,
+            h:testSizeH
+        });
+        done();
+    });     
+    it('we can make it webp', function (done) {
+        new YoupinImg(urlWithSize).setWebp().url.should.equal(`https://${coreUrl}@base@tag=imgScale&h=350&w=550&et=1&eth=${testSizeH}&etw=${testSizeW}&etc=FFFFFF&F=webp`);
+        done();
+    });    
+});
